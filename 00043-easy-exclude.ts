e04d8752bf51af3a8992d6b4359e77f4490e9bcd
ballsona0925 <ballsona0925@naver.com>
2022-11-12 23:25:26 +0900

    add type-challenges solution

---
+++ 00043-easy-exclude.ts
@@ -0,0 +1,21 @@
+/* _____________ Your Code Here _____________ */
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/43/answer
+  > View solutions: https://tsch.js.org/43/solutions
+  > More Challenges: https://tsch.js.org
+*/
