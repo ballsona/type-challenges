42ae2a914b4a552f6e8d9e9a829b9be064fde68a
ballsona <ballsona0925@naver.com>
2023-01-08 22:17:33 -0800

    add type-challenges solution

---
+++ 00015-medium-last.ts
@@ -0,0 +1,18 @@
+/* _____________ Your Code Here _____________ */
+
+type Last<T extends unknown[]> = T extends [...infer F, infer B] ? B : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/15/answer
+  > View solutions: https://tsch.js.org/15/solutions
+  > More Challenges: https://tsch.js.org
+*/
---
+++ 00016-medium-pop.ts
@@ -0,0 +1,19 @@
+/* _____________ Your Code Here _____________ */
+
+type Pop<T extends unknown[]> = T extends [...infer F, infer B] ? F : [];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<[]>, []>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/16/answer
+  > View solutions: https://tsch.js.org/16/solutions
+  > More Challenges: https://tsch.js.org
+*/
