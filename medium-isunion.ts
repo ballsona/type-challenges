49799b3dfdfaa927574213afe5cbbda0597352d1
ballsona <ballsona0925@naver.com>
2023-03-05 12:29:23 +0900

    add type-challenges solution

---
+++ medium-isunion.ts
@@ -0,0 +1,35 @@
+/* _____________ Your Code Here _____________ */
+
+type IsUnion<T, C = T> = [T] extends [never]
+  ? false
+  : T extends C
+  ? [C] extends [T]
+    ? false
+    : true
+  : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | 'a'>, false>>,
+  Expect<Equal<IsUnion<never>, false>>,
+];
+  
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/1097/answer
+  > View solutions: https://tsch.js.org/1097/solutions
+  > More Challenges: https://tsch.js.org
+*/
